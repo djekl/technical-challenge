af6f7cd0bc629c889419bdd32c154d559b839a32
Matthew Davis <matt@mattdavis.co.uk>
2022-07-29 12:02:38 +0100

    Add ISBN Client.

---
+++ app/Client/IsbnClient.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Client;
+
+use Illuminate\Support\Str;
+use Ramsey\Uuid\Uuid;
+
+class IsbnClient
+{
+    public function __construct(
+        private string $username,
+        private string $password
+    ) {
+    }
+
+    public function get(Uuid $uuid): string
+    {
+        // To simulate an expensive HTTP call, we're sleeping here.
+        // In a real system, this would call out to an external service.
+        sleep(3);
+
+        $integer = Str::of($uuid->getInteger());
+
+        return sprintf('%s-%s-%s-%s-%s',
+            $integer->substr(0, 3),
+            $integer->substr(3, 2),
+            $integer->substr(5, 5),
+            $integer->substr(10, 2),
+            $integer->substr(12, 1)
+        );
+    }
+}
